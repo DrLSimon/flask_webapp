9d55cf8c3ebd0dc37ace37021c311832ab0d7960
loic <loic.simon@ensicaen.fr>
2023-03-07 16:21:20 +0100

    first commit

---
+++ webapp/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import os
+import numpy as np
+
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def show_index():
+    all_pairs = np.load('webapp/pairs.npy')
+    #left_image_url = 'https://www.lightingdirect.com/imagebase/resized/x800/Lutronimages/lutron_v_600_al_9.jpg'
+    #right_image_url = 'https://www.lightingdirect.com/imagebase/resized/x800/Lutronimages/lutron_v_600_la_3.jpg'
+    nb_pairs = len(all_pairs)
+    k = int(np.random.choice(nb_pairs, 1))
+    left_image_url, right_image_url = all_pairs[k]
+    return render_template("index.html", left_image_url=left_image_url, right_image_url=right_image_url)
